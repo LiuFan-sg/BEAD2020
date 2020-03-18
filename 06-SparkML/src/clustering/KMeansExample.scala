84713933ed418cb5850570a5e08cf4f24295c052
cloudera <cloudera@quickstart.cloudera>
2020-03-17 20:16:24 -0700

    Update Kmeans

--- 06-SparkML/src/clustering/KMeansExample.scala
+++ 06-SparkML/src/clustering/KMeansExample.scala
@@ -1,5 +1,25 @@
 package clustering
 
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.clustering.KMeans
+
 object KMeansExample {
+  def main(args:Array[String]): Unit = {
+    val spark = SparkSession
+        .builder
+        .master("local[2]")
+        .appName("KMeansExample").getOrCreate()
+    // loads data
+    val dataset = spark.read.format("libsvm").load("data/clustering/sample_kmeans_data.txt")
+    // tain a k-means model
+    val kmeans = new KMeans().setK(2).setSeed(1L)
+    val model = kmeans.fit(dataset)
+    // evaluate clustering by computing within set sum of squared errors
+    val WSSSE = model.computeCost(dataset)
+    println("Cluster centers: ")
+    model.clusterCenters.foreach(println)
+  }
   
 }
