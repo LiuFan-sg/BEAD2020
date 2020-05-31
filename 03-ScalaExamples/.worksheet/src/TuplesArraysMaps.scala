badb31d69c14459cb92dfbc296354a9b6091a0c4
Liu Fan <liufcc@gmail.com>
2020-05-31 10:49:13 +0800

    Delete intermediate files

--- 03-ScalaExamples/.worksheet/src/TuplesArraysMaps.scala
+++
@@ -1,3 +0,0 @@
-object TuplesArraysMaps {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(69); 
-  println("Welcome to the Scala worksheet")}
-}
